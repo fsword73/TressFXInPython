d836d188e17b28d63eb86dcb3722e00b65b3b78e
Jian yang <fsword73@hotmail.com>
2020-06-06 10:59:50 +0800

    Create rgb_hsv.py

---
+++ rgb_hsv.py
@@ -0,0 +1,148 @@
+
+def rgb_to_hsv(arr):
+    """
+    convert float rgb values (in the range [0, 1]), in a numpy array to hsv
+    values.
+
+    Parameters
+    ----------
+    arr : (..., 3) array-like
+       All values must be in the range [0, 1]
+
+    Returns
+    -------
+    hsv : (..., 3) ndarray
+       Colors converted to hsv values in range [0, 1]
+    """
+    # make sure it is an ndarray
+    arr = np.asarray(arr)
+
+    # check length of the last dimension, should be _some_ sort of rgb
+    if arr.shape[-1] != 3:
+        raise ValueError("Last dimension of input array must be 3; "
+                         "shape {shp} was found.".format(shp=arr.shape))
+
+    in_ndim = arr.ndim
+    if arr.ndim == 1:
+        arr = np.array(arr, ndmin=2)
+
+    # make sure we don't have an int image
+    if arr.dtype.kind in ('iu'):
+        arr = arr.astype(np.float32)
+
+    out = np.zeros_like(arr)
+    arr_max = arr.max(-1)
+    ipos = arr_max > 0
+    delta = arr.ptp(-1)
+    s = np.zeros_like(delta)
+    s[ipos] = delta[ipos] / arr_max[ipos]
+    ipos = delta > 0
+    # red is max
+    idx = (arr[..., 0] == arr_max) & ipos
+    out[idx, 0] = (arr[idx, 1] - arr[idx, 2]) / delta[idx]
+    # green is max
+    idx = (arr[..., 1] == arr_max) & ipos
+    out[idx, 0] = 2. + (arr[idx, 2] - arr[idx, 0]) / delta[idx]
+    # blue is max
+    idx = (arr[..., 2] == arr_max) & ipos
+    out[idx, 0] = 4. + (arr[idx, 0] - arr[idx, 1]) / delta[idx]
+
+    out[..., 0] = (out[..., 0] / 6.0) % 1.0
+    out[..., 1] = s
+    out[..., 2] = arr_max
+
+    if in_ndim == 1:
+        out.shape = (3,)
+
+    return out
+
+def hsv_to_rgb(hsv):
+    """
+    convert hsv values in a numpy array to rgb values
+    all values assumed to be in range [0, 1]
+
+    Parameters
+    ----------
+    hsv : (..., 3) array-like
+       All values assumed to be in range [0, 1]
+
+    Returns
+    -------
+    rgb : (..., 3) ndarray
+       Colors converted to RGB values in range [0, 1]
+    """
+    hsv = np.asarray(hsv)
+
+    # check length of the last dimension, should be _some_ sort of rgb
+    if hsv.shape[-1] != 3:
+        raise ValueError("Last dimension of input array must be 3; "
+                         "shape {shp} was found.".format(shp=hsv.shape))
+
+    # if we got pased a 1D array, try to treat as
+    # a single color and reshape as needed
+    in_ndim = hsv.ndim
+    if in_ndim == 1:
+        hsv = np.array(hsv, ndmin=2)
+
+    # make sure we don't have an int image
+    if hsv.dtype.kind in ('iu'):
+        hsv = hsv.astype(np.float32)
+
+    h = hsv[..., 0]
+    s = hsv[..., 1]
+    v = hsv[..., 2]
+
+    r = np.empty_like(h)
+    g = np.empty_like(h)
+    b = np.empty_like(h)
+
+    i = (h * 6.0).astype(np.int)
+    f = (h * 6.0) - i
+    p = v * (1.0 - s)
+    q = v * (1.0 - s * f)
+    t = v * (1.0 - s * (1.0 - f))
+
+    idx = i % 6 == 0
+    r[idx] = v[idx]
+    g[idx] = t[idx]
+    b[idx] = p[idx]
+
+    idx = i == 1
+    r[idx] = q[idx]
+    g[idx] = v[idx]
+    b[idx] = p[idx]
+
+    idx = i == 2
+    r[idx] = p[idx]
+    g[idx] = v[idx]
+    b[idx] = t[idx]
+
+    idx = i == 3
+    r[idx] = p[idx]
+    g[idx] = q[idx]
+    b[idx] = v[idx]
+
+    idx = i == 4
+    r[idx] = t[idx]
+    g[idx] = p[idx]
+    b[idx] = v[idx]
+
+    idx = i == 5
+    r[idx] = v[idx]
+    g[idx] = p[idx]
+    b[idx] = q[idx]
+
+    idx = s == 0
+    r[idx] = v[idx]
+    g[idx] = v[idx]
+    b[idx] = v[idx]
+
+    rgb = np.empty_like(hsv)
+    rgb[..., 0] = r
+    rgb[..., 1] = g
+    rgb[..., 2] = b
+
+    if in_ndim == 1:
+        rgb.shape = (3, )
+
+    return rgb
